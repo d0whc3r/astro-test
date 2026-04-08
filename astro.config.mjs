313c496cf4b9f1dd6d3e4e9573b1c534024ad4cd
Josep Fornies <josep.fornies@qdrant.com>
2026-04-08 21:53:21 +0200

    configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ecoairplastics.com",
+  integrations: [sitemap()],
   build: {
-    format: 'file',
+    format: "file",
   },
   vite: {
     plugins: [tailwindcss()],
